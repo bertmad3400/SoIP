a454260ead94f45115456c11e7f2e27f7020b607
bertmad3400 <skrivtilbertram@gmail.com>
2022-04-11 14:55:44 +0200

    Add function for playing current buffer to client object

--- client.py
+++ client.py
@@ -36,3 +36,28 @@ class Client:
 
         with stream:
             await event.wait()
+
+
+    async def _play_buffer(self):
+        loop = asyncio.get_event_loop()
+        event = asyncio.Event()
+        i = 0
+
+        def callback(outdata, frame_count, time_info, status):
+            nonlocal i
+            if status:
+                logging.info(status)
+            remainder = len(self.buffer) - i
+            if remainder == 0:
+                loop.call_soon_threadsafe(event.set)
+                raise sd.CallbackStop
+
+            valid_frames = frame_count if remainder >= frame_count else remainder
+            outdata[:valid_frames] = self.buffer[i:i + valid_frames]
+            outdata[valid_frames:] = 0
+            i += valid_frames
+
+        stream = sd.OutputStream(callback=callback, dtype=self.buffer.dtype, channels=self.buffer.shape[1])
+
+        with stream:
+            await event.wait()
