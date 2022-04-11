f7f7698743751387d1cd1d838b44abe41ce435a1
bertmad3400 <skrivtilbertram@gmail.com>
2022-04-11 14:54:39 +0200

    Add function for recording audio in client object

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 from common import *
 
+import sounddevice as sd
 import numpy as np
+import asyncio
+
+import logging
 
 class Client:
     def __init__(self):
@@ -8,3 +12,27 @@ class Client:
         self.options = options.SoundOptions
 
         self.buffer = np.empty((self.options.BUFFER_SIZE, self.options.CHANNELS), dtype=self.options.WORD_TYPE)
+
+    async def _record_buffer(self):
+        loop = asyncio.get_event_loop()
+        event = asyncio.Event()
+        i = 0
+
+        def callback(indata, frame_count, time_info, status):
+            nonlocal i
+            if status:
+                logging.info(status)
+
+            remainder = len(self.buffer) - i
+            if remainder == 0:
+                loop.call_soon_threadsafe(event.set)
+                raise sd.CallbackStop
+
+            indata = indata[:remainder]
+            self.buffer[i:i + len(indata)] = indata
+            i += len(indata)
+
+        stream = sd.InputStream(callback=callback, dtype=self.buffer.dtype, channels=self.buffer.shape[1])
+
+        with stream:
+            await event.wait()
