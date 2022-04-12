09cca41606dcb4a4831b692490153843bb9b2e2e
bertmad3400 <skrivtilbertram@gmail.com>
2022-04-12 11:33:33 +0200

    Add as dict method to soundoptions class

--- server/options.py
+++ server/options.py
@@ -3,3 +3,11 @@ class SoundOptions:
     CHANNELS = 2
     WORD_TYPE = "float32"
     BUFFER_SIZE = 150_000
+
+    def as_dict():
+        return {
+                "sample_rate" : SoundOptions.SAMPLE_RATE,
+                "channels" : SoundOptions.CHANNELS,
+                "word_type" : SoundOptions.WORD_TYPE,
+                "buffer_size" : SoundOptions.BUFFER_SIZE
+                }
