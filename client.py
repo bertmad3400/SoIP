5a3b572ef83de03dc72722b551575600e029c0ca
tbjep <tbjepsen1@gmail.com>
2022-04-20 18:07:31 +0200

    Unify audio streams

--- client.py
+++ client.py
@@ -16,7 +16,7 @@ import socket
 from time import sleep
 from datetime import datetime, timedelta
 
-from queue import PriorityQueue
+from queue import PriorityQueue, Queue
 
 class Disconnect(Exception):
     pass
@@ -88,60 +88,14 @@ class Client:
         except BlockingIOError:
             return None
 
-    def record_buffer(self):
-        logging.debug("Recording to buffer...")
-        i = 0
-
-        def callback(indata, frame_count, time_info, status):
-            nonlocal i
-            if status:
-                logging.info(status)
-
-            remainder = len(self.buffer) - i
-            if remainder == 0:
-                raise sd.CallbackStop
-
-            indata = indata[:remainder]
-            self.buffer[i:i + len(indata)] = indata
-            i += len(indata)
-
-        stream = sd.InputStream(callback=callback, dtype=self.buffer.dtype, channels=self.buffer.shape[1])
-
-        with stream:
-            logging.debug("Finished recording to buffer.")
-
-
-    def play_sound(self, sound):
-        logging.debug("Playing sound...")
-        i = 0
-
-        def callback(outdata, frame_count, time_info, status):
-            nonlocal i
-            if status:
-                logging.info(status)
-            remainder = len(sound) - i
-            if remainder == 0:
-                raise sd.CallbackStop
-
-            valid_frames = frame_count if remainder >= frame_count else remainder
-            outdata[:valid_frames] = sound[i:i + valid_frames]
-            outdata[valid_frames:] = 0
-            i += valid_frames
-
-        stream = sd.OutputStream(callback=callback, dtype=sound.dtype, channels=sound.shape[1])
-
-        with stream:
-            logging.debug("Finished playing sound.")
-
-def send_packets(client):
+def send_packets(client, sound_queue):
     while True:
-        if client.muted:
-            if (last_sent_time - datetime.now()) / timedelta(milliseconds=1) > 1000:
+        if client.muted or sound_queue.empty():
+            if (client.last_sent_time - datetime.now()) / timedelta(milliseconds=1) > 1000:
                 client.send_packet(Packet(PacketType.HEARTBEAT, None))
         else:
-            client.record_buffer()
             client.packet_id += 1
-            client.send_packet(Packet(PacketType.SOUND, client.buffer, packet_id=client.packet_id))
+            client.send_packet(Packet(PacketType.SOUND, sound_queue.get_nowait(), packet_id=client.packet_id))
 
 def recieve_packets(client, sound_queue):
     while True:
@@ -168,33 +122,36 @@ class AudioSupplier:
         self.current_fragment_index = 0
 
     def _get_empty_frames(frame_count):
-        return [0] * frame_count
+        return np.empty(frame_count)
 
     def get_frames(self, frame_count):
         if frame_count == 0:
-            return []
+            return np.empty(0)
         if self.current_fragment == None:
             if self.queue.empty():
                 return AudioSupplier._get_empty_frames(frame_count)
             else:
-                current_fragment = self.queue.get_nowait()[1]
+                current_fragment = self.queue.get_nowait()[1].reshape(-1)
                 current_fragment_index = 0
         remainding_frames_in_fragment = len(current_fragment) - current_fragment_index
         if remainding_frames_in_fragment == 0:
             current_fragment = None
             return self.get_frames(frame_count)
 
-        outdata = []
+        outdata = np.empty(frame_count)
         frames_to_write_from_current_fragment = min(frame_count, remainding_frames_in_fragment)
-        outdata.extend(current_fragment[current_fragment_index:current_fragment_index+frames_to_write_from_current_fragment])
+        outdata[:frames_to_write_from_current_fragment] = current_fragment[current_fragment_index:current_fragment_index+frames_to_write_from_current_fragment]
         current_fragment_index += frames_to_write_from_current_fragment
-        outdata.extend(self.get_frames())
+        outdata[frames_to_write_from_current_fragment:] = self.get_frames(frame_count - frames_to_write_from_current_fragment)
 
-def output_sound(client, sound_queue):
-    supplier = AudioSupplier(sound_queue)
-    def callback(outdata, frame_count, time_info, status):
-        outdata = supplier.get_frames(frame_count)
-    with sd.OutputStream(samplerate=client.SAMPLE_RATE, channels=client.CHANNELS, dtype=client.WORD_TYPE, callback=callback):
+        return outdata
+
+def handle_sound(client, input_sound_queue, output_sound_queue):
+    supplier = AudioSupplier(output_sound_queue)
+    def callback(indata, outdata, frame_count, time_info, status):
+        input_sound_queue.put(indata.copy())
+        outdata[:] = supplier.get_frames(frame_count).reshape(-1, 1)
+    with sd.Stream(samplerate=client.SAMPLE_RATE, channels=client.CHANNELS, dtype=client.WORD_TYPE, callback=callback):
         while True: sleep(10)
     os._exit(1)
 
@@ -204,13 +161,14 @@ def main():
 
     client.handshake()
 
-    sound_queue = PriorityQueue()
+    output_sound_queue = PriorityQueue()
+    input_sound_queue = Queue()
 
     threads = {}
 
-    threads["send_packets_thread"] = threading.Thread(target=send_packets, args=(client,), daemon=True)
-    threads["recieve_packets_thread"] = threading.Thread(target=recieve_packets, args=(client, sound_queue), daemon=True)
-    threads["play_sound_thread"] = threading.Thread(target=output_sound, args=(client, sound_queue), daemon=True)
+    threads["send_packets_thread"] = threading.Thread(target=send_packets, args=(client, input_sound_queue), daemon=True)
+    threads["recieve_packets_thread"] = threading.Thread(target=recieve_packets, args=(client, output_sound_queue), daemon=True)
+    threads["play_sound_thread"] = threading.Thread(target=handle_sound, args=(client, input_sound_queue, output_sound_queue), daemon=True)
 
     for thread_name in threads:
         threads[thread_name].start()
